ebf43c6157fdf4af2484842bd86f7bbb9108d060
mulatta <67085791+mulatta@users.noreply.github.com>
2026-08-26 22:43:12 +0900

    voyager: migrate active Oryx layout

--- voyager/config.h
+++ voyager/config.h
@@ -1,3 +1,7 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define SERIAL_NUMBER "VRWgX/LvnZxq"
+#define LAYER_STATE_8BIT
+#define HSS(report) host_system_send(record->event.pressed ? report : 0); return false
+#define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
 
-#pragma once
+#define RGB_MATRIX_STARTUP_SPD 60
--- voyager/rules.mk
+++ voyager/rules.mk
@@ -1,1 +1,7 @@
-# Keep feature flags here when keymap grows beyond ZSA defaults.
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+ORYX_ENABLE = yes
+RGB_MATRIX_CUSTOM_KB = yes
+SPACE_CADET_ENABLE = no
+CAPS_WORD_ENABLE = yes
